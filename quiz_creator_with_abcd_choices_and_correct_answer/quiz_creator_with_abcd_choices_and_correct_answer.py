c1b3bcc0a87ff2d508ecb491cd2b7976fbee381d
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-09 21:24:23 +0800

    Final Output.

--- quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
+++ quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
@@ -1,12 +1,14 @@
 # make an infinite loop that will only stop if the user type "exit"
 # make a txt file that will store the Questions and Answers.
 with open("user_quiz.txt", "a") as file:
+    question_number = 0
 
     while True:
         print("type (exit) if you want to stop adding question")
 
         # ask for a Questions.
         user_questions = input("Enter a Question: ")
+        question_number += 1
 
         if user_questions == "exit":
             print("You typed 'exit'. Exiting the quiz creator.")
@@ -22,7 +24,7 @@ with open("user_quiz.txt", "a") as file:
         correct_answer = input("Enter the correct answer(A, B, C, D): ")
 
     # add the question, possible answers, and the correct answer in a txt file.
-        file.write(f"Question: {user_questions}\n")
+        file.write(f"Question #{question_number}: {user_questions}\n")
         file.write(f"A. {choice_A}\n")
         file.write(f"B. {choice_B}\n")
         file.write(f"C. {choice_C}\n")

b6ef88d46edbbf353ff0abea635065e088be448a
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-09 20:31:43 +0800

    pseudocode/plan is made for Quiz Creator Program.

---
+++ quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
@@ -0,0 +1,6 @@
+# make an infinite loop that will only stop if user type "exit"
+# ask for a Questions.
+# ask for the possible answers in (A, B, C, D)
+# input the correct answer.
+# add the question, possible answers, and the correct answer in a txt file.
+# use the loop if the user wants to add another questions and stop if the user type "exit"

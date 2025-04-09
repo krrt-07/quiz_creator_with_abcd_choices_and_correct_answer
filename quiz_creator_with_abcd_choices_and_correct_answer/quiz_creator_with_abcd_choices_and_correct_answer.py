76350fdbf1b8ed93dffdf6c07295ec03341fd5ef
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-09 21:16:19 +0800

    add the question, possible answers, and the correct answer in a txt file.

--- quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
+++ quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
@@ -1,21 +1,31 @@
 # make an infinite loop that will only stop if the user type "exit"
-while True:
-    print("type (exit) if you want to stop adding question")
+# make a txt file that will store the Questions and Answers.
+with open("user_quiz.txt", "a") as file:
 
-    # ask for a Questions.
-    user_questions = input("Enter a Question: ")
+    while True:
+        print("type (exit) if you want to stop adding question")
 
-    if user_questions == "exit":
-        print("You typed 'exit'. Exiting the quiz creator.")
-        break
+        # ask for a Questions.
+        user_questions = input("Enter a Question: ")
 
-    # ask for the possible answers in (A, B, C, D)
-    choice_A = input("Enter a possible answer, A: ")
-    choice_B = input("Enter a possible answer, B: ")
-    choice_C = input("Enter a possible answer, C: ")
-    choice_D = input("Enter a possible answer, D: ")
+        if user_questions == "exit":
+            print("You typed 'exit'. Exiting the quiz creator.")
+            break
 
-    # input the correct answer.
-    correct_answer = input("Enter the correct answer(A, B, C, D): ")
+        # ask for the possible answers in (A, B, C, D)
+        choice_A = input("Enter a possible answer, A: ")
+        choice_B = input("Enter a possible answer, B: ")
+        choice_C = input("Enter a possible answer, C: ")
+        choice_D = input("Enter a possible answer, D: ")
 
-# add the question, possible answers, and the correct answer in a txt file.
+        # input the correct answer.
+        correct_answer = input("Enter the correct answer(A, B, C, D): ")
+
+    # add the question, possible answers, and the correct answer in a txt file.
+        file.write(f"Question: {user_questions}\n")
+        file.write(f"A. {choice_A}\n")
+        file.write(f"B. {choice_B}\n")
+        file.write(f"C. {choice_C}\n")
+        file.write(f"D. {choice_D}\n")
+        file.write(f"Correct Answer: {correct_answer}\n")
+        file.write("\n") # add space to seperate the Questions.

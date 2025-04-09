0e25fad215d5c9e33e7e2174ea378f30fe6aca8d
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-09 20:36:01 +0800

    make an infinite loop that will only stop if the user type "exit"

--- quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
+++ quiz_creator_with_abcd_choices_and_correct_answer/quiz_creator_with_abcd_choices_and_correct_answer.py
@@ -1,6 +1,12 @@
-# make an infinite loop that will only stop if user type "exit"
-# ask for a Questions.
-# ask for the possible answers in (A, B, C, D)
-# input the correct answer.
+# make an infinite loop that will only stop if the user type "exit"
+while True:
+    print("type (exit) if you want to stop adding question")
+    # ask for a Questions.
+    # ask for the possible answers in (A, B, C, D)
+    # input the correct answer.
+
 # add the question, possible answers, and the correct answer in a txt file.
 # use the loop if the user wants to add another questions and stop if the user type "exit"
+    if user_questions == "exit":
+        print("You typed 'exit'. Exiting the quiz creator.")
+        break
